443ecf427468f2bf34b5dbd9fe8d529b0a6a6076
Krolicc <147572580+Krolicc@users.noreply.github.com>
2023-11-22 15:35:23 +0300

    Update 5.c

--- 5.c
+++ 5.c
@@ -1,229 +1,343 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <Windows.h>
 
-struct Student {
-    unsigned int id;
+typedef enum {
+    SUCCESS = 0,
+    INCORRECT_INPUT,
+    INCORRECT_ARGUMENTS_COUNT,
+    OVERFLOW_ERROR,
+    MALLOC_ERROR,
+    FILE_OPENING_ERROR,
+    FILE_READING_ERROR,
+    MEMORY_ALLOCATION_ERROR,
+    UNKNOWN_ERROR
+} ErrorCode;
+
+static const char* errorMessages[] = {
+        "Всё хорошо",
+        "Некорректный ввод, попробуйте ещё раз",
+        "Неккоректное кол-во данных",
+        "Произошло переполнение",
+        "Проблемы с выделением памяти",
+        "Не удалось открыть файл",
+        "Файл прочитан не полностью",
+        "Ошибка выделения памяти",
+        "Неизвестная ошибка, что-то пошло не так"
+};
+
+typedef struct {
+    int id;
     char* name;
     char* surname;
     char* group;
-    unsigned char* marks;
-};
+    unsigned char* grades;
+} Student;
+
+void freeStudent(Student* student) {
+    free(student->name);
+    free(student->surname);
+    free(student->group);
+    free(student->grades);
+}
 
 int compareById(const void* a, const void* b) {
-    struct Student* studentA = (struct Student*)a;
-    struct Student* studentB = (struct Student*)b;
-
-    return (studentA->id - studentB->id);
+    const Student* studentA = (const Student*)a;
+    const Student* studentB = (const Student*)b;
+    return studentA->id - studentB->id;
 }
 
 int compareBySurname(const void* a, const void* b) {
-    struct Student* studentA = (struct Student*)a;
-    struct Student* studentB = (struct Student*)b;
-
+    const Student* studentA = (const Student*)a;
+    const Student* studentB = (const Student*)b;
     return strcmp(studentA->surname, studentB->surname);
 }
 
 int compareByName(const void* a, const void* b) {
-    struct Student* studentA = (struct Student*)a;
-    struct Student* studentB = (struct Student*)b;
-
+    const Student* studentA = (const Student*)a;
+    const Student* studentB = (const Student*)b;
     return strcmp(studentA->name, studentB->name);
 }
 
 int compareByGroup(const void* a, const void* b) {
-    struct Student* studentA = (struct Student*)a;
-    struct Student* studentB = (struct Student*)b;
-
+    const Student* studentA = (const Student*)a;
+    const Student* studentB = (const Student*)b;
     return strcmp(studentA->group, studentB->group);
 }
 
-void printStudent(struct Student* student) {
-    printf("ID: %u\n", student->id);
-    printf("Фамилия: %s\n", student->surname);
+void printStudent(const Student* student) {
+    printf("\nID: %d\n", student->id);
     printf("Имя: %s\n", student->name);
+    printf("Фамилия: %s\n", student->surname);
     printf("Группа: %s\n", student->group);
 
-    unsigned int sum = 0;
-    for (int i = 0; i < 5; ++i) {
-        sum += student->marks[i];
+    double averageGrade = 0;
+    for (int i = 0; i < 5; i++) {
+        averageGrade += student->grades[i];
     }
-    double average = (double)sum / 5.0;
-
-    printf("Средняя оценка: %.2f\n", average);
-}
-
-void printAboveAverage(struct Student* students, int count, double average) {
-    for (int i = 0; i < count; ++i) {
-        unsigned int sum = 0;
-        for (int j = 0; j < 5; ++j) {
-            sum += students[i].marks[j];
-        }
-        double studentAverage = (double)sum / 5.0;
-
-        if (studentAverage > average) {
-            printf("Фамилия: %s, Имя: %s\n", students[i].surname, students[i].name);
-        }
-    }
+    averageGrade /= 5;
+    printf("Средняя оценка: %.2f\n", averageGrade);
 }
 
 int main(int argc, char* argv[]) {
-    if (argc < 2) {
-        printf("Укажите путь к файлу\n");
-        return 1;
+    SetConsoleCP(1251);
+    SetConsoleOutputCP(1251);
+
+    if (argc != 3) {
+        printf("%s", errorMessages[INCORRECT_ARGUMENTS_COUNT]);
+        return INCORRECT_ARGUMENTS_COUNT;
     }
 
     FILE* file = fopen(argv[1], "r");
-    if (file == NULL) {
-        printf("Ошибка при открытии файла\n");
-        return 1;
+    if (!file) {
+        printf("%s", errorMessages[FILE_OPENING_ERROR]);
+        return FILE_OPENING_ERROR;
     }
 
-    struct Student* students = NULL;
     int count = 0;
+    Student* students = NULL;
 
-    char line[256];
-    while (fgets(line, sizeof(line), file)) {
-        struct Student newStudent;
+    while (!feof(file)) {
+        Student student;
+        fscanf(file, "%d ", &student.id);
 
-        char* token = strtok(line, ",");
-        newStudent.id = atoi(token);
+        int counter = 0, j = 0, k = -1;
+        char fullInfo[1024];
+        char name[100];
+        char surname[100];
+        char group[100];
+        char gradesStr[20] = "";
 
-        token = strtok(NULL, ",");
-        newStudent.name = strdup(token);
+        fgets(fullInfo, sizeof(name), file);
 
-        token = strtok(NULL, ",");
-        newStudent.surname = strdup(token);
+        for(int i=0; i<strlen(fullInfo); i++) {
+            if(fullInfo[i] == ' ') {
+                counter++;
+                j=0;
+                if (counter < 3) continue;
+            }
 
-        token = strtok(NULL, ",");
-        newStudent.group = strdup(token);
-
-        token = strtok(NULL, ",");
-        newStudent.marks = malloc(sizeof(unsigned char) * 5);
-        for (int i = 0; i < 5; ++i) {
-            newStudent.marks[i] = atoi(strtok(token, " "));
-            token = strtok(NULL, ",");
+            switch (counter) {
+                case 0:
+                    name[j] = fullInfo[i];
+                    j++;
+                    name[j] = '\0';
+                    break;
+                case 1:
+                    surname[j] = fullInfo[i];
+                    j++;
+                    surname[j] = '\0';
+                    break;
+                case 2:
+                    group[j] = fullInfo[i];
+                    j++;
+                    group[j] = '\0';
+                    break;
+                default:
+                    if(fullInfo[i] == ' ' && k == -1) break;
+                    k++;
+                    gradesStr[k] = fullInfo[i];
+                    break;
+            }
         }
 
-        students = realloc(students, sizeof(struct Student) * (count + 1));
-        students[count] = newStudent;
+        student.name = strdup(name);
+        student.surname = strdup(surname);
+        student.group = strdup(group);
+        student.grades = (unsigned char*)malloc(5 * sizeof(unsigned char));
+
+        if (student.grades == NULL) {
+            printf("%s", errorMessages[MEMORY_ALLOCATION_ERROR]);
+            fclose(file);
+            return MEMORY_ALLOCATION_ERROR;
+        }
+
+        sscanf(gradesStr, "%hhu%hhu%hhu%hhu%hhu", &student.grades[0],  &student.grades[1],
+               &student.grades[2], &student.grades[3], &student.grades[4]);
+
+        students = (Student*)realloc(students, (count + 1) * sizeof(Student));
+        students[count] = student;
         count++;
     }
 
     fclose(file);
 
-    int option;
-    double average = 0.0;
+    char choice[3];
+    printf("Выберите опцию:\n");
+    printf("0. Поиск по ID\n");
+    printf("1. Поиск по Фамилии\n");
+    printf("2. Поиск по Имени\n");
+    printf("3. Поиск по Группе\n");
+    printf("4. Сортировка по ID\n");
+    printf("5. Сортировка по Фамилии\n");
+    printf("6. Сортировка по Имени\n");
+    printf("7. Сортировка по Группе\n");
+    printf("8. Вывод студента по ID в трассировочный файл\n");
+    printf("9. Вывод студента с оценками выше среднего в трассировочный файл\n");
+    printf("Для выхода введите -1\n");
 
-    while (1) {
-        printf("1. Поиск студента по ID\n");
-        printf("2. Поиск студента по фамилии\n");
-        printf("3. Поиск студента по имени\n");
-        printf("4. Поиск студента по группе\n");
-        printf("5. Сортировка студентов по ID\n");
-        printf("6. Сортировка студентов по фамилии\n");
-        printf("7. Сортировка студентов по имени\n");
-        printf("8. Сортировка студентов по группе\n");
-        printf("9. Вывод данных найденного студента по ID в трассировочный файл\n");
-        printf("10. Вывод фамилий и имен студентов с выше средним баллом в трассировочный файл\n");
-        printf("0. Выход\n");
+//    char traceFilePath[100];
+//    printf("Enter trace file path: ");
+//    fgets(traceFilePath, sizeof(traceFilePath), stdin);
+//    traceFilePath[strcspn(traceFilePath, "\n")] = '\0';
 
-        printf("Выберите опцию: ");
-        scanf("%d", &option);
+    FILE* traceFile = fopen(argv[2], "w");
 
-        if (option == 0) {
-            break;
+    if (!traceFile) {
+        printf("%s", errorMessages[FILE_OPENING_ERROR]);
+        return FILE_OPENING_ERROR;
+    }
+    int numChoice;
+
+    do {
+        printf("Выберите один из вариантов: ");
+        scanf("%s", choice);
+        if(strchr("-0123456789", choice[0]) == NULL) {
+            printf("Неверный выбор.\n");
+            continue;
         }
 
-        switch (option) {
-            case 1: {
-                unsigned int id;
-                printf("Введите ID студента: ");
-                scanf("%u", &id);
+        numChoice = atoi(choice);
 
-                int found = 0;
-                for (int i = 0; i < count; ++i) {
-                    if (students[i].id == id) {
+        switch (numChoice) {
+            case 0: {
+                int searchId;
+                printf("Введите ID: ");
+                scanf("%d", &searchId);
+
+                for (int i = 0; i < count; i++) {
+                    if (students[i].id == searchId) {
                         printStudent(&students[i]);
-                        found = 1;
-                        break;
                     }
                 }
+                break;
+            }
+            case 1: {
+                char searchSurname[100];
+                printf("Введите фамилию: ");
+                scanf("%s", searchSurname);
 
-                if (!found) {
-                    printf("Студент с ID %u не найден\n", id);
+                for (int i = 0; i < count; i++) {
+                    if (strcmp(students[i].surname, searchSurname) == 0) {
+                        printStudent(&students[i]);
+                    }
                 }
                 break;
             }
             case 2: {
-                char surname[256];
-                printf("Введите фамилию студента: ");
-                scanf("%s", surname);
+                char searchName[100];
+                printf("Введите имя: ");
+                scanf("%s", searchName);
 
-                int found = 0;
-                for (int i = 0; i < count; ++i) {
-                    if (strcmp(students[i].surname, surname) == 0) {
+                for (int i = 0; i < count; i++) {
+                    if (strcmp(students[i].name, searchName) == 0) {
                         printStudent(&students[i]);
-                        found = 1;
                     }
-                }
-
-                if (!found) {
-                    printf("Студент с фамилией %s не найден\n", surname);
                 }
                 break;
             }
             case 3: {
-                char name[256];
-                printf("Введите имя студента: ");
-                scanf("%s", name);
+                char searchGroup[100];
+                printf("Введите группу: ");
+                scanf("%s", searchGroup);
 
-                int found = 0;
-                for (int i = 0; i < count; ++i) {
-                    if (strcmp(students[i].name, name) == 0) {
+                for (int i = 0; i < count; i++) {
+                    if (strcmp(students[i].group, searchGroup) == 0) {
                         printStudent(&students[i]);
-                        found = 1;
                     }
                 }
-
-                if (!found) {
-                    printf("Студент с именем %s не найден\n", name);
-                }
                 break;
             }
-            case 4: {
-                char group[256];
-                printf("Введите группу студента: ");
-                scanf("%s", group);
-
-                int found = 0;
-                for (int i = 0; i < count; ++i) {
-                    if (strcmp(students[i].group, group) == 0) {
-                        printStudent(&students[i]);
-                        found = 1;
-                    }
-                }
-
-                if (!found) {
-                    printf("Студенты в группе %s не найдены\n", group);
-                }
+            case 4:
+                qsort(students, count, sizeof(Student), compareById);
+                for (int i = 0; i < count; i++)
+                    printStudent(&students[i]);
                 break;
-            }
-            case 5: {
-                qsort(students, count, sizeof(struct Student), compareById);
-                printf("Коллекция студентов успешно отсортирована по ID\n");
+            case 5:
+                qsort(students, count, sizeof(Student), compareBySurname);
+                for (int i = 0; i < count; i++)
+                    printStudent(&students[i]);
                 break;
-            }
-            case 6: {
-                qsort(students, count, sizeof(struct Student), compareBySurname);
-                printf("Коллекция студентов успешно отсортирована по фамилии\n");
+            case 6:
+                qsort(students, count, sizeof(Student), compareByName);
+                for (int i = 0; i < count; i++)
+                    printStudent(&students[i]);
                 break;
-            }
-            case 7: {
-                qsort(students, count, sizeof(struct Student), compareByName);
-                printf("Коллекция студентов успешно отсортирована по имени\n");
+            case 7:
+                qsort(students, count, sizeof(Student), compareByGroup);
+                for (int i = 0; i < count; i++)
+                    printStudent(&students[i]);
                 break;
-            }
             case 8: {
-                qsort(students, count, sizeof(struct Student), compareByGroup);
-                printf("Коллекция студ
+                int searchId;
+                printf("Введите ID: ");
+                scanf("%d", &searchId);
+
+                for (int i = 0; i < count; i++) {
+                    if (students[i].id == searchId) {
+                        fprintf(traceFile, "ID: %d\n", students[i].id);
+                        fprintf(traceFile, "Имя: %s\n", students[i].name);
+                        fprintf(traceFile, "Фамилия: %s\n", students[i].surname);
+                        fprintf(traceFile, "Группа: %s\n", students[i].group);
+
+                        double averageGrade = 0;
+                        for (int j = 0; j < 5; j++) {
+                            averageGrade += students[i].grades[j];
+                        }
+                        averageGrade /= 5;
+                        fprintf(traceFile, "Средняя оценка: %.2f\n", averageGrade);
+
+                        break;
+                    }
+                }
+                break;
+            }
+            case 9: {
+                double averageGradeSum = 0;
+                int averageGradeCount = 0;
+                for (int i = 0; i < count; i++) {
+                    double averageGrade = 0;
+                    for (int j = 0; j < 5; j++) {
+                        averageGrade += students[i].grades[j];
+                    }
+                    averageGrade /= 5;
+
+                    averageGradeSum += averageGrade;
+                    averageGradeCount++;
+                }
+
+                double averageGradeAverage = averageGradeSum / averageGradeCount;
+                fprintf(traceFile, "Учащиеся с оценками выше среднего:\n");
+
+                for (int i = 0; i < count; i++) {
+                    double averageGrade = 0;
+                    for (int j = 0; j < 5; j++) {
+                        averageGrade += students[i].grades[j];
+                    }
+                    averageGrade /= 5;
+
+                    if (averageGrade > averageGradeAverage) {
+                        fprintf(traceFile, "Имя: %s\n", students[i].name);
+                        fprintf(traceFile, "Фамилия: %s\n", students[i].surname);
+                    }
+                }
+                break;
+            }
+            case -1:
+                break;
+            default:
+                printf("Неверный выбор.\n");
+                break;
+        }
+    } while (numChoice != -1);
+
+    fclose(traceFile);
+
+    for (int i = 0; i < count; i++) {
+        freeStudent(&students[i]);
+    }
+    free(students);
+
+    return SUCCESS;
+}
